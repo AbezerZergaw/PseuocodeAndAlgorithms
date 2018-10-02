c6d54a69f5dd522cb0348fdbcc6bb85a5854d72e
abezer <abezes.haile@gmail.com>
2018-10-02 16:56:03 -0400

    Initial commit

---
+++ src/ATM.java
@@ -0,0 +1,13 @@
+public class ATM {
+
+
+    public void validatePin(int PIN){
+
+    }
+
+    public void validateAccount(int account) {
+
+
+    }
+
+}
---
+++ src/Account.java
@@ -0,0 +1,10 @@
+public class Account {
+
+
+    private String userName;
+    private double balance;
+    private  int PIN;
+    private double deposit;
+    private double withdraws;
+    private int acountNumber;
+}
