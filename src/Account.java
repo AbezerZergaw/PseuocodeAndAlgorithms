f9e7c7702ab477aebf8b72e26fb75d317669066b
Abezer <abezes.haile@gmail.com>
2018-10-05 22:19:59 -0400

    updated file

--- src/Account.java
+++ src/Account.java
@@ -1,10 +1,67 @@
+
+
 public class Account {
 
-
-    private String userName;
+    private String number;
     private double balance;
-    private  int PIN;
-    private double deposit;
-    private double withdraws;
-    private int acountNumber;
+    private String customerNumber;
+    private String customerEmailAddress;
+    private String custerPhoneNumber;
+
+
+    public void deposit(double depositAmount){
+        this.balance +=depositAmount;
+        System.out.println("Deposit of "+ depositAmount + " made. New balance is "+ this.balance);
+    }
+    public void withdrawal(double withdrawalAmount){
+        if(this.balance -withdrawalAmount <0){
+            System.out.println("Only "+ this.balance + " available.");
+        }else {
+            this.balance-=withdrawalAmount;
+            System.out.println("Withdrawal of "+ withdrawalAmount + " processed. Remaining balance = "+ this.balance);
+        }
+
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCusterPhoneNumber() {
+        return custerPhoneNumber;
+    }
+
+    public void setCusterPhoneNumber(String custerPhoneNumber) {
+        this.custerPhoneNumber = custerPhoneNumber;
+    }
+
+
 }
---
+++ src/TestAccount.java
@@ -0,0 +1,16 @@
+public class TestAccount {
+
+    public static void main(String[] args) {
+
+
+        Account bobsAccount = new Account();
+        bobsAccount.withdrawal(100);
+        bobsAccount.deposit(50);
+        bobsAccount.withdrawal(100);
+        bobsAccount.deposit(51);
+        bobsAccount.withdrawal(100);
+
+    }
+
+
+}
